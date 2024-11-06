6f2d152e5c45157b0abce8c8b8414929fc61f957
Gurupriyan j <gurupriyanj007@gmail.com>
2024-11-06 12:54:04 +0530

    remove unwanted files

--- src/components/semntics/ContentArea.js
+++ src/components/semntics/ContentArea.js
@@ -1,20 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom"
 import AllEmployees from '../pages/allBusiness'
-import Leaves from '../pages/Leaves'
-import Attendence from "../pages/Attendence"
-import ShiftAndSchedule from "../pages/ShiftAndSchedule"
 import Clients from "../pages/categories"
-import ClientDetails from "../pages/ClientDetails"
-import ProjectDetailedPage from "../pages/ProjectDetailedPage";
+// import ClientDetails from "../pages/ClientDetails"
 import ProjetsPage from "../pages/plans";
 import PaymentPage from "../pages/PaymentPage";
-import PaySlip from "../PaySlip";
-import Subscription from "../Subscription";
-import Department from "../pages/Department";
-import Designation from "../pages/Designation";
 import SettingAndConfi from "../pages/SettingAndConfi";
-import DeveloperDetails from "../pages/DeveloperDetails";
 import DashBoard from "../pages/DashBoard";
 
 function ContentArea() {
@@ -22,20 +13,11 @@ function ContentArea() {
     <Routes> {/* Use Routes to define all your app routes */}
       <Route path="/" element={<DashBoard />} />
       <Route path="/business" element={<AllEmployees />} />
-      <Route path="/leaves" element={<Leaves />} />
-      <Route path="/attendence" element={<Attendence />} />
-      <Route path="/shiftsandschedules" element={<ShiftAndSchedule />} />
       <Route path="/categories" element={<Clients />} />
-      <Route path="/clientsdetails" element={<ClientDetails />} />
-      <Route path="/detailedprojectpage" element={<ProjectDetailedPage />} />
+      {/* <Route path="/clientsdetails" element={<ClientDetails />} /> */}
       <Route path="/plans" element={<ProjetsPage />} />       
       <Route path="/paymentpage" element={<PaymentPage />} />       
-      <Route path="/payslip" element={<PaySlip />} />   
-      <Route path="/subscription" element={<Subscription />} />   
-      <Route path="/department" element={<Department />} />   
-      <Route path="/designation" element={<Designation />} />   
       <Route path="/settingandconfi" element={<SettingAndConfi />} />   
-      <Route path="/developerdetails" element={<DeveloperDetails />} />   
     </Routes>
   );
 }
